6f49c18b4bfea66f6fc1f85864c58a99e7c02c80
Star12573 <sierra.steck@ucas-edu.net>
2024-09-10 20:28:52 +0000

    added variable-tried at least

--- Variables Practice/helloworld.c
+++ Variables Practice/helloworld.c
@@ -1,9 +1,10 @@
-#include <stdio.h>;
+#include <stdio.h>
 
 int main(void){;
-int("Sierra Steck");
-    printf("Hello! My name is", );
-    printf("and I am 15. I have deep blue eyes and for breakfast I had a fourth of a large chocolate muffin which is a kinda sad breakfast.");
+
+    printf("Hello! My name is Sierra Steck and I am 15.");
+int myNum = 15;  
+    printf("I have deep blue eyes and for breakfast I had a fourth of a large chocolate muffin which is a kinda sad breakfast.");
     printf("My favorite numbers between 1 and 10 and 100 and 1000 are 4 and 214. My favorite color is light blue. I don't know the name, but I know it's lighter than sky blue.");
     printf("I go to UCAS which is Utah County's Academy of Sciences and normally English would be my favorite subject but it's actually Health right now. And Advisory. I'm on student council.");
     return 0;
