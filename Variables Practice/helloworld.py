74775b2f203b16aeeb83a1d9b813d0f771970654
Star12573 <sierra.steck@ucas-edu.net>
2024-09-10 20:20:48 +0000

    included variable

--- Variables Practice/helloworld.py
+++ Variables Practice/helloworld.py
@@ -1,5 +1,6 @@
-name = "Sierra Steck"
-print("Hello! My name is", name)
+x = "Sierra Steck"
+print("Hello! My name is")
+print(x)
 print("My favorite number between 1 and 10 is 4, and my favorite number between 100 and 1000 is 214.")
 print("For breakfast today I had a fourth of a large chocolate muffin, which is honestly a kind of sad breakfast.")
 print("I have too many favorite colors, but I think a light blue is my favorite. Lighter than sky blue, I unfortunately do not know the name.")
