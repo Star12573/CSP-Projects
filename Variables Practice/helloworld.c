c825a1907e9cbfa9e2230721b24a652f6133c360
Star12573 <sierra.steck@ucas-edu.net>
2024-09-11 20:32:26 +0000

    its not working

--- Variables Practice/helloworld.c
+++ Variables Practice/helloworld.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
-int main(void){
-char name[] = "Sierra Steck";
+int main(void) {
+char name[30];
+printf("Pleas tell me your name: \n");
+scanf("%s", name);
 printf("Hello %s", name);
-
 return 0;
 }
