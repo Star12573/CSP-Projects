8f5d5b40c5e9a68c5447449630741dc86a9975d9
Star12573 <sierra.steck@ucas-edu.net>
2024-09-11 20:17:39 +0000

    trying variables

--- Variables Practice/helloworld.c
+++ Variables Practice/helloworld.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void){;
+int main(void){
+char name[] = "Sierra Steck";
+printf("Hello %s", name);
 
-    printf("Hello! My name is Sierra Steck and I am 15.");
-int myNum = 15;  
-    printf("I have deep blue eyes and for breakfast I had a fourth of a large chocolate muffin which is a kinda sad breakfast.");
-    printf("My favorite numbers between 1 and 10 and 100 and 1000 are 4 and 214. My favorite color is light blue. I don't know the name, but I know it's lighter than sky blue.");
-    printf("I go to UCAS which is Utah County's Academy of Sciences and normally English would be my favorite subject but it's actually Health right now. And Advisory. I'm on student council.");
-    return 0;
+return 0;
 }
--- Variables Practice/helloworld.py
+++
@@ -1,8 +0,0 @@
-x = "Sierra Steck"
-print("Hello! My name is")
-print(x)
-print("My favorite number between 1 and 10 is 4, and my favorite number between 100 and 1000 is 214.")
-print("For breakfast today I had a fourth of a large chocolate muffin, which is honestly a kind of sad breakfast.")
-print("I have too many favorite colors, but I think a light blue is my favorite. Lighter than sky blue, I unfortunately do not know the name.")
-print("My school is UCAS; Utah County Academy of Sciences and my favorite subject normally is English, but right now, I think it's health and my Advisory. I'm on Student Council.")
-print("It is 2024 and I am 15, going on 16 in March 2025 and my eyes are blue. Not like a gray blue but a dark blue.")
