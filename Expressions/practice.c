41a54554d452e4bfbc84629863f24766e5277409
Sierra <sierra.steck@ucas-edu.net>
2024-09-24 20:41:09 +0000

    expressions.c

--- Expressions/practice.c
+++ Expressions/practice.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
 
 int main(void){
-    int x, y, w;
-    x = 2;
-    y = 40;
-    w = 4365;
+    int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o;
+    a = 7;
+    b = 24;
+    c = 8;
+    d = 4;
+    e = 6;
+    f = 18;
+    g = 3;
+    h = 2;
+    i = 5;
+    j = 12;
+    k = 72;
+    l = 9;
+    m = 17;
+    n = 15;
+    o = 22;
     printf("%f", x*y);
 return 0;
 }
