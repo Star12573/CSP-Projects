13acd0b4b1eeb10ebfff501333bdbb521a1959c3
Sierra <sierra.steck@ucas-edu.net>
2024-09-25 20:41:59 +0000

    budget.py

--- Expressions/Budget.py
+++ Expressions/Budget.py
@@ -1,15 +1,15 @@
-input("Hello and welcome to your financial calculator! What is your monthly income?: \n")
-input("What is your monthly rent?: \n")
-input("What is your monthly utilities?: \n")
-input("What is your monthly groceries?: \n")
+income = input("Hello and welcome to your financial calculator! What is your monthly income?: \n")
+rent, prent, utilities, putilities = input("What is your monthly rent?: \n")
+groceries, pgroceries, transportation, ptransportation = input("What is your monthly utilities?: \n")
+expenses, pexpenses, savings,psavings= input("What is your monthly groceries?: \n")
 input("What is your monthly transportation costs?: \n")
-f"Your monthly income is ${"income":.2f}\n"
-f"Your monthly expenses are ${"expenses":.2f}\n"
-f"Your monthly savings is ${"savings":.2f}\n"
-f"Your monthly spending money is ${"spending":.2f}\n"
-f"Your rent is {int("prent")}% of your monthly income\n"
-f"Your utilities are {int("putilities")}% of your monthly income\n"
-f"Your groceries are {int("pgrocieries")}% of your monthly income\n"
-f"Your transportation is {int("ptransportation")}% of your monthly income\n"
-f"Your savings are {int("psavings")}% of your monthly income\n"
-f"Your expenses are {int("pexpenses")}% of your monthly income\n"
+print(f"Your monthly income is ${"income":.2f}\n")
+print(f"Your monthly expenses are ${"expenses":.2f}\n")
+print(f"Your monthly savings is ${"savings":.2f}\n")
+print(f"Your monthly spending money is ${"spending":.2f}\n")
+print(f"Your rent is {int("prent")}% of your monthly income\n")
+print(f"Your utilities are {int("putilities")}% of your monthly income \n")
+print(f"Your groceries are {int("pgrocieries")}% of your monthly income \n")
+print(f"Your transportation is {int("ptransportation")}% of your monthly income \n")
+print(f"Your savings are {int("psavings")}% of your monthly income \n")
+print(f"Your expenses are {int("pexpenses")}% of your monthly income \n")
--- Expressions/practice.c
+++ Expressions/practice.c
@@ -21,11 +21,11 @@ int main(void){
     p = 1;
     printf("%f", (float) 7-24/8*4+6);
     printf("%f", (float) 18/3-7+2*5);
-    printf("%f", (float) 6*4/12+72/8-9)
+    printf("%f", (float) 6*4/12+72/8-9);
     printf("%f", (float) (17-6/2)+4*3);
     printf("%f", (float) -2*(1*4-2/2)+(6+2-3));
     printf("%f", (float) -1*((3-4*7)/5)-2*24/6);
-    printf("%f", (float) (3*5*pow(2,2)/15)-(5-2*pow(2,2));
+    printf("%f", (float) (3*5*pow(2,2)/15)-(5-2*pow(2,2)));
     printf("%f", (float) (1*pow(4,4)*2*pow(2,2)+3*pow(3,3))-2*pow(5,5)/4);
     printf("%f", (float) (22/2-2*5)*pow(2,2)+(4-6/6));
 return 0;
