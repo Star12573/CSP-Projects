7983f528de6db659ffa147eb6588f84dc4487380
Sierra <sierra.steck@ucas-edu.net>
2024-09-25 01:57:06 +0000

    budget.py

---
+++ Expressions/Budget.py
@@ -0,0 +1,15 @@
+input("Hello and welcome to your financial calculator! What is your monthly income?: \n")
+input("What is your monthly rent?: \n")
+input("What is your monthly utilities?: \n")
+input("What is your monthly groceries?: \n")
+input("What is your monthly transportation costs?: \n")
+f"Your monthly income is ${"income":.2f}\n"
+f"Your monthly expenses are ${"expenses":.2f}\n"
+f"Your monthly savings is ${"savings":.2f}\n"
+f"Your monthly spending money is ${"spending":.2f}\n"
+f"Your rent is {int("prent")}% of your monthly income\n"
+f"Your utilities are {int("putilities")}% of your monthly income\n"
+f"Your groceries are {int("pgrocieries")}% of your monthly income\n"
+f"Your transportation is {int("ptransportation")}% of your monthly income\n"
+f"Your savings are {int("psavings")}% of your monthly income\n"
+f"Your expenses are {int("pexpenses")}% of your monthly income\n"
