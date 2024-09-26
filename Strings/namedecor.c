b4487c35bd5029beb50bae0afb5d33f446bc1053
Sierra <sierra.steck@ucas-edu.net>
2024-09-26 20:41:30 +0000

    namedecor.c

---
+++ Strings/namedecor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    char name[30];
+    printf("Please tell me your name: \n");
+    fgets(name,sizeof(name), stdin );
+    printf("Hello %s", name);
+
+    printf("Please tell me your name: \n");
+    fgets(name,sizeof(name), stdin );
+    printf("Hello %s", name);
+
+    
+    return 0;
+}
