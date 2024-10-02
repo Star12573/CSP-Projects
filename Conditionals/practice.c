8c81286bdf269be3d39f15f9a1be15a307300063
Sierra <sierra.steck@ucas-edu.net>
2024-10-02 20:26:32 +0000

    time.c

--- Conditionals/practice.c
+++ Conditionals/practice.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){;
+int grade;
 
+int main(){
+    printf("What is your grade percentage?\n");
+    scanf("%d", &grade);
+    if (grade >= 90){
+        printf("You have an A!\n");
+    }else if (grade >= 80){
+        printf("You have a B!\n");
+    }else if (grade >= 70){
+        printf("You have a C.\n");
+    }else if (grade >= 60){
+        printf("You have a D. :(\n");
+    }else{
+        printf("You have an F. T^T");
+    }
+    
 return 0;
 }
