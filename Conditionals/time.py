87d51c87cb86f84948faee9e9ad203bb6b675f38
Sierra <sierra.steck@ucas-edu.net>
2024-10-05 17:58:56 +0000

    time

--- Conditionals/time.py
+++ Conditionals/time.py
@@ -1,6 +1,14 @@
 import datetime
 
 now = datetime.datetime.now()
-
+if "hour <= 12":
+    print("Good Morning!")
+if "hour >12":
+    print("Good Afternoon!")
+if "hour >=18":
+    print("Good Evening!")
+else:
+    print("Good Night!")
 print(now)
+
 
--- Variables Practice/practice.c
+++ Variables Practice/practice.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    int name, num, numTwo, cereal, color, colorTwo, colorThree, school, year, age, subject;
-    name = "Sierra Steck";
-    num = 4;
-    numTwo = 214;
-    cereal = "Captain Crunch";
-    color = "soft blue";
-    colorTwo = "baby blue";
-    colorThree = "blue";
-    school = "UCAS; Utah County Academy of Sciences";
-    year = 2024;
-    age = 15;
-    subject = "Health";
+    char name[30];
+    printf("Please tell me your name: \n");
+    fgets(name,sizeof(name), stdin );
+    printf("Hello %s", name);
+    
+
     return 0;
 }
