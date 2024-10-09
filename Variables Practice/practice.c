a693c690a202de94e6db45c4a85e77dabfa0d2a3
Sierra <sierra.steck@ucas-edu.net>
2024-10-09 20:42:02 +0000

    name

--- Variables Practice/practice.c
+++ Variables Practice/practice.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-    char name[30];
-    printf("Please tell me your name: \n");
-    fgets(name,sizeof(name), stdin );
-    printf("Hello %s", name);
+    n
+    char name[] = "Sierra Steck";
+    num = 4;
+    numTwo = 214;
+    cereal = "Captain Crunch";
+    color = "soft blue";
+    colorTwo = "baby blue";
+    colorThree = "blue";
+    school = "UCAS; Utah County Academy of Sciences";
+    year = 2024;
+    age = 15;
+    subject = "Health";
+    printf("Hello! My name is", name,"!");
+    printf("My favorite numbers between 1 and 10 and 100 and 1,000 are:", num, "and", numTwo,".");
+    printf("For breakfast, I had", cereal,".");
+    printf("My favorite color is", color, "or", colorTwo, "or just any kind of", colorThree, "really, I go to school at", school,", and it is currently", year,".");
+    printf("My eyes are", colorThree, ", I am", age,", and my favorite subject in school right now might very well be", subject,".");
     
 
     return 0;
