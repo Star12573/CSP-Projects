2bca1784e8583d2882dc81d2188b629d6a46787b
Sierra <sierra.steck@ucas-edu.net>
2024-10-10 20:19:36 +0000

    buzz

--- Loops/fizzbuzz.c
+++ Loops/fizzbuzz.c
@@ -1,9 +1,24 @@
 #include<stdio.h>
-
-int main(void){;
+int i;
+int main(void){
     //create a loop that counts to 50
+    for(i=1; i<=50; i++){
+        
 
-
-    
+        //replace #'s divisible by 3 and 5 with "FizzBuzz"
+        if(i%3==0 && i%5==0){
+            printf("FizzBuzz\n");
+            //replace #'s divisible by 3 with "Fizz"
+        }else if (i%3==0){
+        printf("Fizz\n");
+        //replace #'s divisible by 5 with "Buzz"
+        }else if (i%5==0){
+            printf("Buzz\n");
+        //print all other numbers
+        }else{
+        
+        printf("%d\n", i);
+        }
+    }
 return 0;
 }
--- Variables Practice/practice.c
+++ Variables Practice/practice.c
@@ -13,11 +13,11 @@ int main(void) {
     year = 2024;
     age = 15;
     subject = "Health";
-    printf("Hello! My name is", name,"!");
-    printf("My favorite numbers between 1 and 10 and 100 and 1,000 are:", num, "and", numTwo,".");
-    printf("For breakfast, I had", cereal,".");
-    printf("My favorite color is", color, "or", colorTwo, "or just any kind of", colorThree, "really, I go to school at", school,", and it is currently", year,".");
-    printf("My eyes are", colorThree, ", I am", age,", and my favorite subject in school right now might very well be", subject,".");
+    printf("Hello! My name is", name,"!\n");
+    printf("My favorite numbers between 1 and 10 and 100 and 1,000 are:", num, "and", numTwo,".\n");
+    printf("For breakfast, I had", cereal,".\n");
+    printf("My favorite color is", color, "or", colorTwo, "or just any kind of", colorThree, "really, I go to school at", school,", and it is currently", year,".\n");
+    printf("My eyes are", colorThree, ", I am", age,", and my favorite subject in school right now might very well be", subject,".\n");
     
 
     return 0;
