c416bffad65d613237d11d525e543b06cef02fda
Sierra <sierra.steck@ucas-edu.net>
2024-10-10 20:42:02 +0000

    codew/me

--- Loops/fizzbuzz.c
+++ Loops/fizzbuzz.c
@@ -1,24 +1,45 @@
-#include<stdio.h>
-int i;
-int main(void){
-    //create a loop that counts to 50
-    for(i=1; i<=50; i++){
-        
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int i, usr;
+char one[50], two[50], three[50];
 
+void delay(int sec){
+    int milli = 1000*sec;
+
+    clock_t start = clock();
+
+    while(clock()< start+milli);
+}
+
+int main(void){
+    printf("Give me a #: \n");
+    scanf("%d", &usr);
+    printf("Give me a word: \n");
+    scanf("%s", one);
+    printf("Give me another word: \n");
+    scanf("%s", two);
+    strcat(three, one);
+    strcat(three, two);
+    //create a loop that counts to 50
+    for(i=1; i<=usr; i++){
         //replace #'s divisible by 3 and 5 with "FizzBuzz"
         if(i%3==0 && i%5==0){
-            printf("FizzBuzz\n");
+            printf("%s\n", three);
             //replace #'s divisible by 3 with "Fizz"
         }else if (i%3==0){
-        printf("Fizz\n");
+        printf("%s\n", one);
+        
         //replace #'s divisible by 5 with "Buzz"
         }else if (i%5==0){
-            printf("Buzz\n");
+            printf("%s\n", two);
+
         //print all other numbers
         }else{
         
         printf("%d\n", i);
         }
+        delay(2);
     }
 return 0;
 }
