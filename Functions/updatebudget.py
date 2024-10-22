209423a57cd62c59f4c72f05cfc6d7e39aa2b3d0
Sierra <sierra.steck@ucas-edu.net>
2024-10-22 20:20:11 +0000

    update

--- Functions/updatebudget.py
+++ Functions/updatebudget.py
@@ -1,16 +1,11 @@
-def budget(type, int):
-    budget = int, type
-    
-input("How much is your {type}?: \n")
-
-income = ("Hello and welcome to your financial calculator! What is your monthly income?: \n")
-rent = ("What is your monthly rent?: \n")
-utilities = ("What is your monthly utilities?: \n")
-groceries = ("What is your monthly groceries?: \n")
-transportation = ("What is your monthly transportation costs?: \n")
-expenses = rent+transportation+groceries+utilities
+income = float (input("Hello and welcome to your financial calculator! What is your monthly income?: \n"))
+rent = float (input("What is your monthly rent?: \n"))
+utilities = float (input("What is your monthly utilities?: \n"))
+groceries = float (input("What is your monthly groceries?: \n"))
+transportation = float (input("What is your monthly transportation costs?: \n"))
+expenses = rent + transportation + groceries + utilities
 savings = income*2
-spending = income-expenses-savings
+spending = income - expenses - savings
 def percent(type, amount):
     per = amount/income*100
 
